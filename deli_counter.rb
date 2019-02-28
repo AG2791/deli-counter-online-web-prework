6b62058adb10bbf8ffd4c4ca2aff83023f054000
AJ Guiles <ashiguiles@gmail.com>
2019-02-28 06:35:37 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,5 +1,3 @@
-katz_deli = []
-
 def line(katz_deli) 
    if katz_deli.length == 0 
      puts "The line is currently empty."
@@ -8,14 +6,23 @@ def line(katz_deli)
    end
 end 
 
-return line(katz_deli) 
+
 
 def take_a_number(katz_deli,name)
-  name=gets.chomp 
+  puts "Please put your name on the list."
+    name=gets.chomp 
+    katz_deli.push(name)
+    position= katz_deli.index(name) + 1
+  puts  "Welcome, #{name} You are number #{position} in line"
+  end
 
-  katz_deli.push(name)
-  puts katz_deli.index(name-1)
-  
-end 
-  
-  
+
+def now_serving (katz_deli)
+    if katz_deli.length == 0
+        puts "There is nobody waiting to be served!"
+    else     
+    name = katz_deli.first  
+    puts "Now Serving #{name}!"
+     katz_deli.shift
+    end
+end
