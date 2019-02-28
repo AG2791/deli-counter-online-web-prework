a6f071e087b5b8dac9e2d08488bfb2e8f3423790
AJ Guiles <ashiguiles@gmail.com>
2019-02-28 04:49:25 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,10 +1,11 @@
 katz_deli = []
 
 def line(katz_deli) 
-   if katz_deli.length == 0
+   if katz_deli.length == 0 
      puts "The line is currently empty."
    else 
      puts katz_deli
    end
- end
-  return line(katz_deli)
+end 
+
+return line(katz_deli) 
